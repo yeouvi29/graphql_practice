d982e9f2f0e04114ca7ecc0544d063b2e1e8992e
Emily Kang <yeouvi29@gmail.com>
2024-06-13 17:14:18 -0700

    feat: add client-side code of hello-world

---
+++ hello-world/client/app.js
@@ -0,0 +1,18 @@
+async function fetchGreeting() {
+  const response = await fetch("http://localhost:9000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: "query { greeting }",
+    }),
+  });
+  const { data } = await response.json();
+  console.log("data:", data);
+  return data.greeting;
+}
+
+fetchGreeting().then((greeting) => {
+  document.getElementById("greeting").textContent = greeting;
+});
